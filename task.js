a62fa7f5a1aba49db04a74ed37859550099bbfd2
Anjali197 <anjalics101@gmail.com>
2022-10-17 16:12:39 +0530

    My commit

---
+++ task.js
@@ -0,0 +1,5 @@
+let toogle=document.querySelector("menu-icon");
+let menu=document.querySelector("responsive-menu-list ul li");
+toogle.addEventListener('click',function(){
+    alert("clicked")
+})
